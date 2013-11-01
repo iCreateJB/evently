be20db75e9bfc540ec3b714a0176e97a28d7e2ff
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-11-01 15:49:01 -0400

    Adding redis connection.

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app     = express();
+var redis   = require('redis');
+    client  = redis.createClient(process.env['REDIS_PORT'],process.env['REDIS_HOST']);
+    
+    client.on("error", function (err) {
+      console.log("error event - " + client.host + ":" + client.port + " - " + err);
+    });
 
 app.set('json spaces',0)
 app.get('/', function(req,res) {
+  client.set("galileo:ip", req.connection.remoteAddress, redis.print);
   res.json({ip: req.connection.remoteAddress});
 });
 
