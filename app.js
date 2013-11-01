d9ab40d6067c3dc5620d638a8a37c62902ddada0
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-11-01 16:16:37 -0400

    Adding redis password.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app     = express();
 var redis   = require('redis');
     client  = redis.createClient(process.env['REDIS_PORT'],process.env['REDIS_HOST']);
+    client.auth(process.env['REDIS_AUTH_PASS']);
     
     client.on("error", function (err) {
       console.log("error event - " + client.host + ":" + client.port + " - " + err);
