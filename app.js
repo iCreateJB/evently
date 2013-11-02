cba5d32cf9aa67ac97c27c6e15c9f6b94b087db9
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-11-01 20:07:42 -0400

    Update logic to find correct IP.

--- app.js
+++ app.js
@@ -5,13 +5,26 @@ var moment  = require('moment');
 
 app.set('json spaces',0)
 app.get('/', function(req,res) {
-  redis.set("galileo:ip:"+req.connection.remoteAddress+":"+timestamp(), req.connection.remoteAddress);
-  redis.sadd("galileo:ip:"+req.connection.remoteAddress,"galileo:ip:"+req.connection.remoteAddress+":"+timestamp());
-  res.json({ip: req.connection.remoteAddress});
+  redis.set("galileo:ip:"+getIp(req)+":"+timestamp(), getIp(req));
+  redis.sadd("galileo:ip:"+getIp(req),"galileo:ip:"+getIp(req)+":"+timestamp());
+  res.json({ip: getIp(req)});
 });
 
 function timestamp(){
   return moment().format('YYYY-MM-DD');
+}
+
+function getIp(req){
+  var ipAddress;
+  var forwardedIpsStr = req.header('x-forwarded-for');
+  if (forwardedIpsStr){
+    var forwardedIps = forwardedIpsStr.split(',');
+    ipAddress = forwardedIps[0];
+  }
+  if (!ipAddress) {
+    ipAddress= req.connection.remoteAddress;
+  }
+  return ipAddress;
 }
 
 app.listen(process.env.PORT || 5000);
