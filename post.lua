31e58c237458cdf53301502073a836e768ad8743
Jonathan Ballard <jonathan.ballard@gmail.com>
2014-07-26 12:50:33 -0400

    Benchmarking test script.

---
+++ post.lua
@@ -0,0 +1,3 @@
+wrk.method = "POST"
+wrk.body   = '{"api_key":"070c3402-48bc-477f-aadd-adacf0fa3d38","app_id":"1e3d408e-088d-4503-a01a-422a1df750ef","status":"Info","error":"NoMethodError (undefined method `uuid` for #<DataGathering::SummaryTransactions:0x000000069063b0>):"}'
+wrk.headers["Content-Type"] = "application/json"
